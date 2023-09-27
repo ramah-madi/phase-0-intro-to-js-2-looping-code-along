b8ca0cf2b48f002d41ec3506caeee56ed0aa6f0c
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2023-09-28 02:13:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const newArray = []
+function writeCards(array, event) {
+    for(let count = 0; count<array.length; count++) {
+       newArray.push(`Thank you, ${array[count]}, for the wonderful ${event} gift!`)
+       
+    }
+    return newArray
+}
+
+
+function countDown(number) {
+    while( 0 <= number){
+        console.log(number);
+        number--;
+    }
+}
